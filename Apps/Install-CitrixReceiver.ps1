5d2e13e63740e347f892e20c356194027b26b6e2
Aaron Parker <aaron@stealthpuppy.com>
2018-01-06 16:40:00 -0800

    Added Get-ReceiverVersion function; Updated logging
    Added Get-ReceiverVersion function that gets the latest release from
    the public download page; Added Start-Transcript to enable simpler
    logging.

--- Apps/Install-CitrixReceiver.ps1
+++ Apps/Install-CitrixReceiver.ps1
@@ -3,16 +3,47 @@
     Downloads and installs Citrix Receiver (Win32/Desktop version) for full functionality.
     Allows for Receiver installation via a PowerShell script to Windows 10 with Microsoft Intune.
     Provides basic error checking and outputs to a log file; Add -Verbose for running manually.
-#>
 
-# Variables
+    .NOTES
+        Name: Install-CitrixReceiver.ps1
+        Author: Aaron Parker
+        Site: https://stealthpuppy.com
+        Twitter: @stealthpuppy
+#>
+# Common variables
+$VerbosePreference = "Continue"
+$LogFile = "$env:SystemRoot\Temp\$($MyInvocation.MyCommand.Name).log"
+Start-Transcript -Path $LogFile -Append
+
+Function Get-ReceiverVersion {
+    Param(
+        [string]$Url = 'https://www.citrix.com/downloads/citrix-receiver/windows/receiver-for-windows-latest.html'
+    )
+
+    # RegEx to find version number in format Major.Minor.Build
+    $RegEx = "(\d+\.)(\d+\.)(\d+)"
+
+    # Read the HTML page and filter for meta tags
+    Write-Verbose "Reading Citrix Receiver for Windows public download page for current release."
+    $Html = Invoke-WebRequest -Uri $Url
+    $Content = $Html.ParsedHtml.getElementsByTagName("meta") | Select-Object content
+
+    # Match meta tags with software build number in the text; Remove all alpha characters leaving just the build number
+    $Version = $Content.content -match $RegEx
+    $Build = $Version[0] -replace "[^0-9.]"
+
+    # Output the build nunmber to the pipeline
+    Write-Verbose "Citrix Receiver current release is: $Build."
+    Write-Output $Build
+}
+
+# Receiver variables
 $Url = "https://downloadplugins.citrix.com/Windows/CitrixReceiver.exe"
 $Target = "$env:SystemRoot\Temp\CitrixReceiver.exe"
 $TargetWeb = "$env:SystemRoot\Temp\CitrixReceiverWeb.exe"; $Rename = $True
-$LogFile = "$env:SystemRoot\Temp\CitrixReceiver.log"
-$BaselineVersion = [System.Version]"4.10.1.0"
+$BaselineVersion = [System.Version](Get-ReceiverVersion)
+# $BaselineVersion = [System.Version]"4.10.1.0"
 $Arguments = "/AutoUpdateCheck=auto /AutoUpdateStream=Current /DeferUpdateCount=5 /AURolloutPriority=Medium /NoReboot /Silent EnableCEIP=False"
-$VerbosePreference = "Continue"
 
 # Determine whether Receiver is already installed
 Write-Verbose -Message "Querying for installed Receiver version."
@@ -24,26 +55,23 @@ If (!($Receiver) -or ($Receiver.Version -lt $BaselineVersion)) {
     # Win32 Receiver and Receiver for Store can't coexist. Remove Store version if installed
     # https://docs.citrix.com/en-us/receiver/windows-store/current-release/install.html
     Write-Verbose -Message "Querying for Receiver for Store."
-    Get-AppxPackage -AllUsers | Where-Object { $_.Name -like "D50536CD.CitrixReceiver" } | Remove-AppxPackage -AllUsers -ErrorAction SilentlyContinue -ErrorVariable $ErrorRemoveAppx -Verbose
-    If ($ErrorRemoveAppx) { $ErrorRemoveAppx | Out-File -FilePath $LogFile -Append }
+    Get-AppxPackage -AllUsers | Where-Object { $_.Name -like "D50536CD.CitrixReceiver" } | Remove-AppxPackage -AllUsers -ErrorAction Continue -ErrorVariable $ErrorRemoveAppx -Verbose
 
     # Install the .NET Framework 3.5. This will download the .NET Framework from the Internet
     # Citrix Receiver system requirements: https://docs.citrix.com/en-us/receiver/windows/current-release/system-requirements.html
     Write-Verbose -Message "Querying for required .NET Framework."
     If ((Get-WindowsCapability -Online -Name "NetFx3~~~~").State -ne "Installed") {
         Write-Verbose -Message "Installing .NET Framework 3.5"
-        Add-WindowsCapability -Online -Name "NetFx3~~~~" -ErrorAction SilentlyContinue -ErrorVariable $ErrorAddDotNet -Verbose
-        If ($ErrorAddDotNet) { $ErrorAddDotNet | Out-File -FilePath $LogFile -Append }
+        Add-WindowsCapability -Online -Name "NetFx3~~~~" -ErrorAction Continue -ErrorVariable $ErrorAddDotNet -Verbose
     }
 
     # Delete the installer if it exists, so that we don't have issues downloading
-    If (Test-Path $Target) { Write-Verbose -Message "Deleting $Target"; Remove-Item -Path $Target -Force -ErrorAction SilentlyContinue -Verbose }
+    If (Test-Path $Target) { Write-Verbose -Message "Deleting $Target"; Remove-Item -Path $Target -Force -ErrorAction Continue -Verbose }
 
     # Download Citrix Receiver locally; This should succeed, because the machine must have Internet access to receive the script from Intune
     # Will download regardless of network cost state (i.e. if network is marked as roaming, it will still download); Likely won't support proxy servers
     Write-Verbose -Message "Downloading Citrix Receiver from $Url"
-    Start-BitsTransfer -Source $Url -Destination $Target -Priority High -TransferPolicy Always -ErrorAction SilentlyContinue -ErrorVariable $ErrorBits -Verbose
-    If ($ErrorBits) { $ErrorBits | Out-File -FilePath $LogFile -Append }
+    Start-BitsTransfer -Source $Url -Destination $Target -Priority High -TransferPolicy Always -ErrorAction Continue -ErrorVariable $ErrorBits -Verbose
     
     # If $Rename is True, rename the executable. Renaming to CitrixReceiverWeb.exe supresses the Add Account window without having to set /ALLOWADDSTORE=N
     If ($Rename) { Write-Verbose -Message "Renaming $Target to $TargetWeb"; Rename-Item -Path $Target -NewName $TargetWeb; $Target = $TargetWeb }
@@ -52,14 +80,12 @@ If (!($Receiver) -or ($Receiver.Version -lt $BaselineVersion)) {
     If (Test-Path $Target) {
         Write-Verbose -Message "Installing Citrix Receiver."; Start-Process -FilePath $Target -ArgumentList $Arguments -Wait
         Write-Verbose -Message "Sleeping for 3 seconds."; Start-Sleep -Seconds 3
-        Write-Verbose -Message "Deleting $Target"; Remove-Item -Path $Target -Force -ErrorAction SilentlyContinue -Verbose
+        Write-Verbose -Message "Deleting $Target"; Remove-Item -Path $Target -Force -ErrorAction Continue -Verbose
         Write-Verbose -Message "Querying for installed Receiver version."
         $Receiver = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -Like "Citrix Receiver Inside*" } | Select-Object Name, Version
-        Write-Verbose -Message "Installed Receiver: $($Receiver.Version)."
-        "[$(Get-Date)] Installed Citrix Receiver: $($Receiver.Version)" | Out-File -FilePath $LogFile -Append
+        Write-Verbose -Message "Installed Citrix Receiver: $($Receiver.Version)."
     } Else {
         $ErrorInstall = "Citrix Receiver installer path at $Target not found."
-        $ErrorInstall | Out-File -FilePath $LogFile -Append
     }
 
     # Intune shows basic deployment status in the Overview blade of the PowerShell script properties
@@ -67,3 +93,5 @@ If (!($Receiver) -or ($Receiver.Version -lt $BaselineVersion)) {
 } Else {
     Write-Verbose "Skipping Receiver installation. Installed version is $($Receiver.Version)"
 }
+
+Stop-Transcript
