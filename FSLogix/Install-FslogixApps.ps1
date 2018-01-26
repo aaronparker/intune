31906f9404a273ff1c8a480b8ba5e431fcec1bbc
Aaron Parker <aaron@stealthpuppy.com>
2018-01-26 23:19:20 +1100

    Added create scheduled task; Variables updates

--- FSLogix/Install-FslogixApps.ps1
+++ FSLogix/Install-FslogixApps.ps1
@@ -19,9 +19,16 @@
 # Variables
 $VerbosePreference = "Continue"
 $LogFile = "$env:ProgramData\stealthpuppy\Logs\$($MyInvocation.MyCommand.Name).log"
-$Target = "$env:SystemRoot\Temp"
-$Arguments = "/install /quiet /norestart ProductKey=TRIAL-G6KID-WKRKO-J96IA-O9SB7"
 Start-Transcript -Path $LogFile
+
+$Target = "$env:ProgramData\stealthpuppy\Scripts"
+$ScriptUrl = "https://raw.githubusercontent.com/aaronparker/intune/master/FSLogix/Get-FslogixRuleset.ps1"
+$Script = Split-Path -Path $ScriptUrl -Leaf
+$TaskName = "Get FSLogix Ruleset"
+$Group = "NT AUTHORITY\SYSTEM"
+$Execute = "powershell.exe"
+$ScriptArguments = "-ExecutionPolicy Bypass -NonInteractive -WindowStyle Minimized -File $Target\$Script"
+$InstallerArguments = "/install /quiet /norestart ProductKey=TRIAL-G6KID-WKRKO-J96IA-O9SB7"
 
 # Set installer download URL based on processor architecture
 Switch ((Get-WmiObject Win32_OperatingSystem).OSArchitecture) {
@@ -30,48 +37,68 @@ Switch ((Get-WmiObject Win32_OperatingSystem).OSArchitecture) {
 }
 
 # Download FSLogix Agent installer; Get file info from the downloaded file to compare against what's installed
-$Installer = Split-Path -Path $Url -Leaf
-Write-Verbose -Message "Downloading $Url to $Target\$Installer"
-Start-BitsTransfer -Source $Url -Destination "$Target\$Installer" -Priority High -TransferPolicy Always -ErrorAction Continue -ErrorVariable $ErrorBits
-$ProductVersion = (Get-ItemProperty -Path "$Target\$Installer").VersionInfo.ProductVersion
-If ($ProductVersion) { Write-Verbose "Downloaded FSLogix Apps version: $ProductVersion." } Else { Write-Verbose "Unable to query downloaded FSLogix Apps version." }
+$Installer = "$env:SystemRoot\Temp\$(Split-Path -Path $Url -Leaf)"
+Write-Verbose -Message "Downloading $Url to $Installer"
+Start-BitsTransfer -Source $Url -Destination $Installer -Priority High -TransferPolicy Always -ErrorAction Continue -ErrorVariable $ErrorBits
+$ProductVersion = (Get-ItemProperty -Path $Installer).VersionInfo.ProductVersion
+If ($ProductVersion) { Write-Verbose -Message "Downloaded FSLogix Apps version: $ProductVersion." } Else { Write-Verbose -Message "Unable to query downloaded FSLogix Apps version." }
 
 # Determine whether FSLogix Agent is already installed
 Write-Verbose -Message "Querying for installed FSLogix Apps version."
 $Agent = Get-WmiObject -Class Win32_Product -ErrorAction Continue | Where-Object { $_.Name -Like "FSLogix Apps" } | Select-Object Name, Version
-If ($Agent) { Write-Verbose "Found FSLogix Apps $($Agent.Version)." }
+If ($Agent) { Write-Verbose -Message "Found FSLogix Apps $($Agent.Version)." }
 
 # Install the FSLogix Agent
-If (Test-Path "$Target\$Installer") {
+If (Test-Path $Installer) {
 
     # If installed version less than downloaded version, install the update
     If (!($Agent) -or ($Agent.Version -lt $ProductVersion)) {
         Write-Verbose -Message "Installing the FSLogix Agent $ProductVersion."; 
-        Start-Process -FilePath "$Target\$Installer" -ArgumentList $Arguments -Wait
-        Write-Verbose -Message "Deleting $Target\$Installer"; Remove-Item -Path "$Target\$Installer" -Force -ErrorAction Continue
+        Start-Process -FilePath $Installer -ArgumentList $InstallerArguments -Wait
+        Write-Verbose -Message "Deleting $Installer"; Remove-Item -Path $Installer -Force -ErrorAction Continue
         Write-Verbose -Message "Querying for installed FSLogix Agent."
         $Agent = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -Like "FSLogix Apps" } | Select-Object Name, Version
         Write-Verbose -Message "Installed FSLogix Agent: $($Agent.Version)."
     } Else {
 
         # Skip install if agent already installed and up to date
-        Write-Verbose "Skipping installation of the FSLogix Agent. Version $($Agent.Version) already installed."
+        Write-Verbose -Message "Skipping installation of the FSLogix Agent. Version $($Agent.Version) already installed."
     }
 } Else {
-    Write-Verbose "Unable to find the FSLogix Apps installer."
+    Write-Verbose -Message "Unable to find the FSLogix Apps installer."
     # If we get here, it's possible the script couldn't download the installer
     # Delete script key under HKLM\SOFTWARE\Microsoft\IntuneManagementExtension to get script to re-run again in ~60 minutes
     $KeyParent = "HKLM:\SOFTWARE\Microsoft\IntuneManagementExtension\Policies"
     $ScriptName = Split-Path -Path $MyInvocation.MyCommand.Name -Leaf
     $KeyPath = "$KeyParent\$($ScriptName.Split("_")[0])\$($ScriptName.Split("_")[1] -replace ".ps1")"
     If (Test-Path -Path $KeyPath) {
-        Write-Verbose "Removing registry key to force script to re-run: $KeyPath"
+        Write-Verbose -Message "Removing registry key to force script to re-run: $KeyPath"
         Remove-Item -Path $KeyPath -Force
     }
     Stop-Transcript
     Break
 }
 
-# Add configure scheduled task here.
+# Download the script that will download FSLogix ruleset files
+If (!(Test-Path -Path $Target)) { New-Item -Path $Target -ItemType Directory }
+Start-BitsTransfer -Source $ScriptUrl -Destination "$Target\$Script" -Priority High -TransferPolicy Always -ErrorAction Continue -ErrorVariable $ErrorBits
+
+# Create a scheduled task to run the script
+Write-Verbose -Message "Creating folder redirection scheduled task."
+# Build a new task object
+$action = New-ScheduledTaskAction -Execute $Execute -Argument $ScriptArguments -Verbose
+$trigger =  New-ScheduledTaskTrigger -AtStartup
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -Hidden -DontStopIfGoingOnBatteries -Compatibility Win8 -Verbose
+$principal = New-ScheduledTaskPrincipal -GroupId $Group -Verbose
+$newTask = New-ScheduledTask -Action $action -Trigger $trigger -Settings $settings -Principal $principal -Verbose
+
+# No task object exists, so register the new task
+Register-ScheduledTask -InputObject $newTask -TaskName $TaskName -Verbose
+
+# Get the task properties and set the trigger duration and interval
+$Task = Get-ScheduledTask -TaskName $TaskName
+$Task.Triggers.Repetition.Duration = "PT12H"
+$Task.Triggers.Repetition.Interval = "PT2H"
+$Task | Set-ScheduledTask -User $Group
 
 Stop-Transcript
