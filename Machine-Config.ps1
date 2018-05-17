b5cccfb4a579889f695afcf77967119a4fb11396
Aaron Parker <aaron@stealthpuppy.com>
2018-05-17 15:08:44 +1000

    Script updates

--- Machine-Config.ps1
+++ Machine-Config.ps1
@@ -12,7 +12,7 @@
 #>
 [CmdletBinding()]
 Param (
-    [Parameter()]$LogFile = "$env:ProgramData\stealthpuppy\Logs\$($MyInvocation.MyCommand.Name).log"
+    [Parameter()] $LogFile = "$env:ProgramData\stealthpuppy\Logs\$($MyInvocation.MyCommand.Name).log"
 )
 
 # Start logging
@@ -21,10 +21,12 @@ Start-Transcript -Path $LogFile
 Function New-RegValue {
     [CmdletBinding()]
     Param (
-        [Parameter(Mandatory = $True)]$Key,
-        [Parameter(Mandatory = $True)]$Value,
-        [Parameter(Mandatory = $True)]$Data,
-        [Parameter(Mandatory = $True)][ValidateSet('Binary','ExpandString','String','Dword','MultiString','QWord')]$Type
+        [Parameter(Mandatory = $True)] $Key,
+        [Parameter(Mandatory = $True)] $Value,
+        [Parameter(Mandatory = $True)] $Data,
+        [Parameter(Mandatory = $True)]
+        [ValidateSet('Binary', 'ExpandString', 'String', 'Dword', 'MultiString', 'QWord')]
+        $Type
     )
     If (!(Test-Path $Key)) { New-Item -Path $Key -Force }
     New-ItemProperty -Path $Key -Name $Value -Value $Data -PropertyType $Type -Force
@@ -37,7 +39,20 @@ New-RegValue -Key "HKLM:\SOFTWARE\Policies\Microsoft\OneDrive" -Value "FilesOnDe
 
 # Install .NET Framework 3.5
 $State = Get-WindowsCapability -Online | Where-Object { $_.Name -like "NetFx3~~~~" -and $_.State -eq "NotPresent" }
-If ($State) { Add-WindowsCapability –Online -Name "NetFx3~~~~" -Verbose }
+If ($State) { Add-WindowsCapability -Online -Name "NetFx3~~~~" -Verbose }
+
+# Remove capabilities
+Get-WindowsCapability -Online | Where-Object { $_.Name -like "Browser.InternetExplorer~~~~*" } | Remove-WindowsCapability -Online
+Get-WindowsCapability -Online | Where-Object { $_.Name -like "Media.WindowsMediaPlayer~~~~*" } | Remove-WindowsCapability -Online
+
+# Remove other optional features
+$Features = @("WorkFolders-Client", `
+        "Internet-Explorer-Optional-amd64", `
+        "Microsoft-Windows-Printing-XPSServices-Package", `
+        "Printing-XPSServices-Features", `
+        "WindowsMediaPlayer", `
+        "FaxServicesClientPackage")
+Disable-WindowsOptionalFeature -Online -FeatureName $Features -NoRestart -Verbose
 
 # Stop Logging
 Stop-Transcript
