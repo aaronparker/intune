a8fe6c414737a2fc6d00616750121b2155b882b4
Aaron Parker <aaron@stealthpuppy.com>
2018-10-15 22:51:51 +1100

    Script updates

--- Machine-Config/Set-AllowDeviceNameInTelemetry.ps1
+++ Machine-Config/Set-AllowDeviceNameInTelemetry.ps1
@@ -1,7 +1,12 @@
 # Enable AllowDeviceNameInTelemetry for Windows 10 1803 devices. Currently unable to do this via CSP
 # https://docs.microsoft.com/en-au/windows/deployment/update/windows-analytics-get-started
 
+$LogFile = "$env:LocalAppData\Intune-PowerShell-Logs\AllowDeviceNameInTelemetry.log"
+Start-Transcript -Path $LogFile
+
 $registryPath = "HKLM:\Software\Policies\Microsoft\Windows\DataCollection"
 $name = "AllowDeviceNameInTelemetry"
 $value = "1"
 New-ItemProperty -Path $registryPath -Name $name -Value $value -PropertyType DWORD -Force | Out-Null
+
+Stop-Transcript
--- OneDrive/OneDrive-UserConfig.ps1
+++ OneDrive/OneDrive-UserConfig.ps1
@@ -22,61 +22,79 @@ Function New-RegValue {
     New-ItemProperty -Path $Key -Name $Value -Value $Data -PropertyType $Type -Force
 }
 
+Function Update-OneDrive {
+    Param (
+        [Parameter()] $CurrentVersion,
+        [Parameter()] $OldVersion,
+        [Parameter()] $OneDrivePath,
+        [Parameter()] $OneDriveStandaloneUpdater
+    )
+
+    # OneDriveUpdater path
+    $OneDriveUpdater = Join-Path -Path $OneDrivePath -ChildPath $OneDriveStandaloneUpdater
+
+    # Get current installed version
+    $folder = Get-ChildItem -Path $OneDrivePath | Where-Object { $_.Name -like "17.*" -or $_.Name -like "18.*" } `
+        | Sort-Object -Descending | Select-Object -First 1
+
+    # If installed version is old, let's update it
+    # On first logon, need to wait for OneDrive to install
+    If ([System.Version]$folder.Name -le $OldVersion) {
+        For ($i = 1; $i -le 10; $i++) {
+            If (Test-Path $OneDriveUpdater) { Break }
+            Start-Sleep -Seconds 10
+        }
+
+        # Once installed, force the updater to run
+        Start-Process -FilePath $OneDriveUpdater
+
+        # Wait while the installer does its thing
+        While ([System.Version]$folder.Name -le $CurrentVersion) {
+            Start-Sleep -Seconds 10
+            $folder = Get-ChildItem -Path $OneDrivePath | Where-Object { $_.Name -like "18.*" } `
+                | Sort-Object -Descending | Select-Object -First 1
+            If ([System.Version]$folder.Name -ge $CurrentVersion) { Break }
+        }
+    }
+}
+
+Function Install-OneDrive {
+    Param (
+        [Parameter()] $CurrentVersion,
+        [Parameter()] $OneDriveUrl = "https://go.microsoft.com/fwlink/?linkid=844652",
+        [Parameter()] $OneDriveInstaller = "$env:Temp\OneDriveSetup.exe"
+    )
+
+    $OneDriveRegistry = Get-ItemProperty -Path "HKCU:\Software\Microsoft\OneDrive" -Name "Version"
+    If ([System.Version]$OneDriveRegistry.Version -lt [System.Version]$CurrentVersion) {
+        
+        Invoke-WebRequest -Uri $OneDriveUrl -OutFile $OneDriveInstaller
+        If (Test-Path $OneDriveInstaller) {
+            Start-Sleep -Seconds 5
+            Start-Process -FilePath $OneDriveInstaller -ArgumentList "/silent" -Wait
+            Start-Sleep -Seconds 5
+        }
+        Else {
+            Write-Error -Message "OneDrive setup failed to download."
+        }
+    }
+}
+
 $LogFile = "$env:LocalAppData\Intune-PowerShell-Logs\OneDrive-UserConfig.log"
 Start-Transcript -Path $LogFile
 
 # Creates the EnableADAL registry value for silent account config
 New-RegValue -Key "HKCU:\SOFTWARE\Microsoft\OneDrive" -Value "EnableADAL" -Data "1" -Type "Dword"
 
-
-# Attempt to update OneDrive; Variables
+# Update OneDrive
+$CurrentVersion = [System.Version]"18.151.0729.0012"
+$OldVersion = [System.Version]"17.3.6816.0313"
 $OneDrivePath = "$env:LocalAppData\Microsoft\OneDrive"
 $OneDriveStandaloneUpdater = "OneDriveStandaloneUpdater.exe"
-$OneDriveUpdater = Join-Path -Path $OneDrivePath -ChildPath $OneDriveStandaloneUpdater
-$1709Version = [System.Version]"17.3.6816.0313"
-$CurrentVersion = [System.Version]"18.151.0729.0012"
 
-# Get current installed version
-$folder = Get-ChildItem -Path $OneDrivePath | Where-Object { $_.Name -like "17.*" -or $_.Name -like "18.*" } `
-    | Sort-Object -Descending | Select-Object -First 1
+Update-OneDrive -CurrentVersion $CurrentVersion -OldVersion $OldVersion `
+    -OneDrivePath $OneDrivePath -OneDriveStandaloneUpdater $OneDriveStandaloneUpdater
 
-# If installed version is old, let's update it
-# On first logon, need to wait for OneDrive to install
-If ([System.Version]$folder.Name -le $1709Version) {
-    For ($i = 1; $i -le 10; $i++) {
-        If (Test-Path $OneDriveUpdater) { Break }
-        Start-Sleep -Seconds 10
-    }
-
-    # Once installed, force the updater to run
-    Start-Process -FilePath $OneDriveUpdater
-
-    # Wait while the installer does its thing
-    While ([System.Version]$folder.Name -le $CurrentVersion) {
-        Start-Sleep -Seconds 10
-        $folder = Get-ChildItem -Path $OneDrivePath | Where-Object { $_.Name -like "18.*" } `
-            | Sort-Object -Descending | Select-Object -First 1
-        If ([System.Version]$folder.Name -ge $CurrentVersion) { Break }
-    }
-}
-
-<#
-# If update hasn't worked, try download and install latest OneDrive update
-$OneDriveRegistry = Get-ItemProperty -Path "HKCU:\Software\Microsoft\OneDrive" -Name "Version"
-If ([System.Version]$OneDriveRegistry.Version -lt [System.Version]$CurrentVersion) {
-    $OneDriveUrl = "https://go.microsoft.com/fwlink/?linkid=844652"
-    $OneDriveInstaller = "$env:Temp\OneDriveSetup.exe"
-    
-    Invoke-WebRequest -Uri $OneDriveUrl -OutFile $OneDriveInstaller
-    If (Test-Path $OneDriveInstaller) {
-        Start-Sleep -Seconds 5
-        Start-Process -FilePath $OneDriveInstaller -ArgumentList "/silent" -Wait
-        Start-Sleep -Seconds 5
-    }
-    Else {
-        Write-Error -Message "OneDrive setup failed to download."
-    }
-}
-#>
+Install-OneDrive -CurrentVersion $CurrentVersion
 
 Stop-Transcript
