3c024a0ca3ffc8945c8cc19eae9f1d29b8265fc2
Aaron Parker <aaron@stealthpuppy.com>
2019-03-15 21:46:47 +1100

    Add Test-WindowsVersion
    Add Test-WindowsVersion function; Update Storage Sense

---
+++ Functions/Test-WindowsVersion.ps1
@@ -0,0 +1,29 @@
+Function Test-WindowsVersion {
+    <#
+        .SYNOPSIS
+            Creates a registry value in a target key. Creates the target key if it does not exist.
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $False)]
+        [ValidateSet('17763', '17134', '16299', '15063', '14393', '10240')]
+        [string] $Build = "17763",
+
+        [Parameter(Mandatory = $False)]
+        [ValidateSet('Higher', 'Lower', 'Match')]
+        [string] $Test = "Higher"
+    )
+
+    $currentBuild = [Environment]::OSVersion.Version.Build
+    Switch ($Test) {
+        "Higher" {
+            If ($currentBuild -gt $Build) { Write-Output $True } Else { Write-Output $False }
+        }
+        "Lower" {
+            If ($currentBuild -lt $Build) { Write-Output $True } Else { Write-Output $False }
+        }
+        "Match" {
+            If ($currentBuild -eq $Build) { Write-Output $True } Else { Write-Output $False }
+        }
+    }
+}
--- Storage-Sense/Set-StorageSense.ps1
+++ Storage-Sense/Set-StorageSense.ps1
@@ -11,6 +11,37 @@
         https://stealthpuppy.com
 #>
 
+Function Test-WindowsVersion {
+    <#
+        .SYNOPSIS
+            Creates a registry value in a target key. Creates the target key if it does not exist.
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $False)]
+        [ValidateSet('17763', '17134', '16299', '15063', '14393', '10240')]
+        [string] $Build = "17763",
+
+        [Parameter(Mandatory = $False)]
+        [ValidateSet('Higher', 'Lower', 'Match')]
+        [string] $Test = "Higher"
+    )
+
+    $currentBuild = [Environment]::OSVersion.Version.Build
+    Switch ($Test) {
+        "Higher" {
+            If ($currentBuild -gt $Build) { Write-Output $True } Else { Write-Output $False }
+        }
+        "Lower" {
+            If ($currentBuild -lt $Build) { Write-Output $True } Else { Write-Output $False }
+        }
+        "Match" {
+            If ($currentBuild -eq $Build) { Write-Output $True } Else { Write-Output $False }
+        }
+    }
+}
+
+
 Function Set-RegistryValue {
     <#
         .SYNOPSIS
@@ -66,41 +97,48 @@ Function Set-RegistryValue {
     If ($val.Property -contains $Value) {
         Write-Verbose "Write value success: $Value"
         Write-Output $True
-    } Else {
+    }
+    Else {
         Write-Verbose "Write value failed."
         Write-Output $False
     }
 }
 
-
-
+#region Actions
 $stampDate = Get-Date
 $scriptName = ([System.IO.Path]::GetFileNameWithoutExtension($(Split-Path $script:MyInvocation.MyCommand.Path -Leaf)))
 $logFile = "$env:LocalAppData\Intune-PowerShell-Logs\$scriptName-" + $stampDate.ToFileTimeUtc() + ".log"
 Start-Transcript -Path $logFile
 
-# Ensure the StorageSense key exists
-$key = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy"
+If (Test-WindowsVersion -Build "17134" -Test "Higher") {
+    Write-Verbose "This version of Windows supports Storage Sense via OMA-URI custom settings. Exiting."
+}
+ELse {
 
-# Set Storage Sense settings
-# Enable Storage Sense
-Set-RegistryValue -Key $key -Value "01" -Type DWord -Data 1 -Verbose
+    # Ensure the StorageSense key exists
+    $key = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\StorageSense\Parameters\StoragePolicy"
 
-# Set 'Run Storage Sense' to Every Week
-Set-RegistryValue -Key $key -Value "2048" -Type DWord -Data 7 -Verbose
+    # Set Storage Sense settings
+    # Enable Storage Sense
+    Set-RegistryValue -Key $key -Value "01" -Type DWord -Data 1 -Verbose
 
-# Enable 'Delete temporary files that my apps aren't using'
-Set-RegistryValue -Key $key -Value "04" -Type DWord -Data 1 -Verbose
+    # Set 'Run Storage Sense' to Every Week
+    Set-RegistryValue -Key $key -Value "2048" -Type DWord -Data 7 -Verbose
 
-# Set 'Delete files in my recycle bin if they have been there for over' to 60 days
-Set-RegistryValue -Key $key -Value "08" -Type DWord -Data 1 -Verbose
-Set-RegistryValue -Key $key -Value "256" -Type DWord -Data 60 -Verbose
+    # Enable 'Delete temporary files that my apps aren't using'
+    Set-RegistryValue -Key $key -Value "04" -Type DWord -Data 1 -Verbose
 
-# Set 'Delete files in my Downloads folder if they have been there for over' to 60 days
-Set-RegistryValue -Key $key -Value "32" -Type DWord -Data 1 -Verbose
-Set-RegistryValue -Key $key -Value "512" -Type DWord -Data 60 -Verbose
+    # Set 'Delete files in my recycle bin if they have been there for over' to 60 days
+    Set-RegistryValue -Key $key -Value "08" -Type DWord -Data 1 -Verbose
+    Set-RegistryValue -Key $key -Value "256" -Type DWord -Data 60 -Verbose
 
-# Set value that Storage Sense has already notified the user
-Set-RegistryValue -Key $key -Value "StoragePoliciesNotified" -Type DWord -Data 1 -Verbose
+    # Set 'Delete files in my Downloads folder if they have been there for over' to 60 days
+    Set-RegistryValue -Key $key -Value "32" -Type DWord -Data 1 -Verbose
+    Set-RegistryValue -Key $key -Value "512" -Type DWord -Data 60 -Verbose
+
+    # Set value that Storage Sense has already notified the user
+    Set-RegistryValue -Key $key -Value "StoragePoliciesNotified" -Type DWord -Data 1 -Verbose
+}
 
 Stop-Transcript
+#endregion
