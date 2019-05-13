ff7ac4529d75044216e892627d015319fd377fca
Aaron Parker <aaron@stealthpuppy.com>
2019-05-13 10:50:43 +1000

    Query & download templates from Azure

--- Uev/Set-Uev.ps1
+++ Uev/Set-Uev.ps1
@@ -60,6 +60,65 @@ Param (
     [string] $Uri
 )
 
+# Configure
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+#region Functions
+Function Get-AzureBlobItems {
+    <#
+        .SYNOPSIS
+            Returns an array of items and properties from an Azure blog storage URL.
+
+        .DESCRIPTION
+            Queries an Azure blog storage URL and returns an array with properties of files in a Container.
+            Requires Public access level of anonymous read access to the blob storage container.
+            Works with PowerShell Core.
+            
+        .NOTES
+            Author: Aaron Parker
+            Twitter: @stealthpuppy
+
+        .LINK
+            https://stealthpuppy.com
+
+        .PARAMETER Url
+            The Azure blob storage container URL. The container must be enabled for anonymous read access.
+            The URL must include the List Container request URI. See https://docs.microsoft.com/en-us/rest/api/storageservices/list-containers2 for more information.
+        
+        .EXAMPLE
+            Get-AzureBlobItems -Url "https://aaronparker.blob.core.windows.net/folder/?comp=list"
+
+            Description:
+            Returns the list of files from the supplied URL, with Name, URL, Size and Last Modifed properties for each item.
+
+        .OUTPUTS
+            Returns System.Array
+    #>
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    Param (
+        [Parameter(ValueFromPipeline = $True, Mandatory = $True, HelpMessage = "Azure blob storage URL with List Containers request URI '?comp=list'.")]
+        [ValidatePattern("^(http|https)://")]
+        [string] $Uri
+    )
+
+    # Get response from Azure blog storage; Convert contents into usable XML, removing extraneous leading characters
+    Try { $list = Invoke-WebRequest -Uri $Uri -ErrorAction Stop } Catch [Exception] { Write-Host $_; Break }
+    [xml] $xml = $list.Content.Substring($list.Content.IndexOf("<?xml", 0))
+
+    # Build an object with file properties to return on the pipeline
+    $output = @()
+    ForEach ($node in (Select-Xml -XPath "//Blobs/Blob" -Xml $Xml).Node) {
+        $item = New-Object -TypeName PSObject
+        $item | Add-Member -Type NoteProperty -Name 'Name' -Value ($node | Select-Object -ExpandProperty Name)
+        $item | Add-Member -Type NoteProperty -Name 'Url' -Value ($node | Select-Object -ExpandProperty Url)
+        $item | Add-Member -Type NoteProperty -Name 'Size' -Value ($node | Select-Object -ExpandProperty Size)
+        $item | Add-Member -Type NoteProperty -Name 'LastModified' -Value ($node | Select-Object -ExpandProperty LastModified)
+        $output += $item
+    }
+    Write-Output $output
+}
+#endregion
+
 # If the UEV module is installed, enable the UEV service
 If (Get-Module -ListAvailable -Name UEV) {
     Import-Module -Name UEV
@@ -115,9 +174,31 @@ If ($status.UevEnabled -eq $True) {
     Set-UevConfiguration @UevParams
 }
 
-# Inbox templates
+# Templates local target path
 $inboxTemplatesSrc = "$env:ProgramData\Microsoft\UEV\InboxTemplates"
-$inboxTemplates = Get-ChildItem -Path $inboxTemplatesSrc -Filter "*.xml" | Select-Object -Property FullName
-ForEach ($template in $inboxTemplates) {
-    Register-UevTemplate -Path $template
+
+# Copy the UEV templates from an Azure Storage account
+If (Test-Path -Path $inboxTemplatesSrc) {
+    
+    # Retrieve the list of templates from the Azure Storage account
+    $srcTemplates = Get-AzureBlobItems -Uri "https://stealthpuppy.blob.core.windows.net/uevtemplates/?comp=list"
+
+    # Download each template to the target path
+    ForEach ($template in $srcTemplates) {
+        # If (Test-Path -Path "$inboxTemplatesSrc\$(Split-Path -Path $template -Leaf)") { Remove-Item -Path "$inboxTemplatesSrc\$(Split-Path -Path $template -Leaf)" -Force }
+        Invoke-WebRequest -Uri $template.Url -OutFile "$inboxTemplatesSrc\$(Split-Path -Path $template.Url -Leaf)" -UseBasicParsing -Headers @{ "x-ms-version" = "2015-02-21" }
+    }
+
+    # Unregister existing templates
+    Get-UevTemplate | Unregister-UevTemplate -ErrorAction SilentlyContinue
+
+    # Register specified templates [Need to make this list more robust]
+    $templates = @("AdobeReaderDC.xml", "MicrosoftNotepad.xml", "MicrosoftOffice2016Win64.xml", `
+            "MicrosoftOutlook2016CAWin64.xml", "MicrosoftSkypeForBusiness2016Win64.xml", "MicrosoftWordpad.xml")
+    ForEach ($template in $templates) {
+        Register-UevTemplate -Path "$inboxTemplatesSrc\$template"
+    }
+}
+Else {
+    Write-Warning "Path does not exist: $inboxTemplatesSrc."
 }
