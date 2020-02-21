3c1cf62f6b799cf9076fa8c777c1acbeef413979
Aaron Parker <aaron@stealthpuppy.com>
2020-02-21 21:17:21 +1100

    Add Get-AzureBlobItem

---
+++ Uev/Get-AzureBlobItem.ps1
@@ -0,0 +1,68 @@
+Function Get-AzureBlobItem {
+    <#
+        .SYNOPSIS
+            Returns an array of items and properties from an Azure blog storage URL.
+
+        .DESCRIPTION
+            Queries an Azure blog storage URL and returns an array with properties of files in a Container.
+            Requires Public access level of anonymous read access to the blob storage container.
+            Works with PowerShell Core.
+            
+        .NOTES
+            Author: Aaron Parker
+            Twitter: @stealthpuppy
+
+        .PARAMETER Url
+            The Azure blob storage container URL. The container must be enabled for anonymous read access.
+            The URL must include the List Container request URI. See https://docs.microsoft.com/en-us/rest/api/storageservices/list-containers2 for more information.
+        
+        .EXAMPLE
+            Get-AzureBlobItems -Uri "https://aaronparker.blob.core.windows.net/folder/?comp=list"
+
+            Description:
+            Returns the list of files from the supplied URL, with Name, URL, Size and Last Modifed properties for each item.
+    #>
+    [CmdletBinding(SupportsShouldProcess = $False)]
+    [OutputType([System.Management.Automation.PSObject])]
+    Param (
+        [Parameter(ValueFromPipeline = $True, Mandatory = $True, HelpMessage = "Azure blob storage URL with List Containers request URI '?comp=list'.")]
+        [ValidatePattern("^(http|https)://")]
+        [System.String] $Uri
+    )
+
+    # Get response from Azure blog storage; Convert contents into usable XML, removing extraneous leading characters
+    try {
+        $iwrParams = @{
+            Uri             = $Uri
+            UseBasicParsing = $True
+            ContentType     = "application/xml"
+            ErrorAction     = "Stop"
+        }
+        $list = Invoke-WebRequest @iwrParams
+    }
+    catch [System.Net.WebException] {
+        Write-Warning -Message ([string]::Format("Error : {0}", $_.Exception.Message))
+    }
+    catch [System.Exception] {
+        Write-Warning -Message "$($MyInvocation.MyCommand): failed to download: $Uri."
+        Throw $_.Exception.Message
+    }
+    If ($Null -ne $list) {
+        [System.Xml.XmlDocument] $xml = $list.Content.Substring($list.Content.IndexOf("<?xml", 0))
+
+        # Build an object with file properties to return on the pipeline
+        $fileList = New-Object -TypeName System.Collections.ArrayList
+        ForEach ($node in (Select-Xml -XPath "//Blobs/Blob" -Xml $xml).Node) {
+            $PSObject = [PSCustomObject] @{
+                Name         = ($node | Select-Object -ExpandProperty Name)
+                Url          = ($node | Select-Object -ExpandProperty Url)
+                Size         = ($node | Select-Object -ExpandProperty Size)
+                LastModified = ($node | Select-Object -ExpandProperty LastModified)
+            }
+            $fileList.Add($PSObject) | Out-Null
+        }
+        If ($Null -ne $fileList) {
+            Write-Output -InputObject $fileList
+        }
+    }
+}
