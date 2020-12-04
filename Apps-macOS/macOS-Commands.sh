08038c4ff1ebef83d22dc1a7886eb93d610ff508
Aaron Parker <aaron@stealthpuppy.com>
2020-12-05 08:50:12 +1100

    Cleanup

--- Apps-macOS/macOS-Commands.sh
+++ Apps-macOS/macOS-Commands.sh
@@ -1,5 +1,2 @@
 # Find package identifiers
 osascript -e 'id of app "Microsoft OneDrive"'
-
-# Convert plist to mobileConfig
-~/Temp/mcxToProfile.py --plist ~/Projects/Intune-Scripts/Apps/com.microsoft.Edge.plist --identifier com.Microsoft.Edge
