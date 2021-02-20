e43229c45ec803194673b1763639faf91fa07f1a
Aaron Parker <aaron@stealthpuppy.com>
2021-02-20 20:30:41 +1100

    Update/Add README; script cleanup

--- AzureAD/Export-StateDevices.ps1
+++ AzureAD/Export-StateDevices.ps1
@@ -1,7 +1,19 @@
+#Requires -Modules AzureADP
+<#
+    .SYNOPSIS
+        List users and devices including last login time stamp.
 
+    .NOTES
+        Author: Aaron Parker
+        Twitter: @stealthpuppy
+
+    .LINK
+        https://stealthpuppy.com
+#>
+[CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Low')]
+Param ()
 
 $Users = Get-AzureADUser -All $true | Select-Object UserPrincipalName, ObjectId
-
 ForEach ($User in $Users) {
     Get-AzureADUserRegisteredDevice -ObjectId $user.ObjectId | ForEach-Object {
         $Output = [PSCustomObject] @{
@@ -13,7 +25,6 @@ ForEach ($User in $Users) {
         Write-Output -InputObject $Output
     }
 }
-
 
 #$dt = [datetime]’2017/01/01’
 #Get-MsolDevice -all -LogonTimeBefore $dt | select-object -Property Enabled, DeviceId, DisplayName, DeviceTrustType, ApproximateLastLogonTimestamp | export-csv devicelist-olderthan-Jan-1-2017-summary.csv
---
+++ AzureAD/README.md
@@ -0,0 +1,6 @@
+# Azure AD scripts
+
+Scripts for Azure AD configuration:
+
+* `Import-AzureADDynamicGroups.ps1` - import a set of Azure AD dynamic device groups from a CSV file
+* `Export-StateDevices.ps1` - List users and devices including last login time stamp
